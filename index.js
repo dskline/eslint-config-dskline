72e9c280a8651f4b42b52738081389ee754b5167
dskline <dskline1@gmail.com>
2019-04-04 01:23:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+
+  parser: "@typescript-eslint/parser",
+
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:compat/recommended",
+    "plugin:jest/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:promise/recommended",
+    "plugin:react/recommended",
+    "react-app",
+    "standard",
+    "standard-react"
+  ],
+
+  plugins: [
+    "@typescript-eslint",
+    "babel",
+    "compat",
+    "import",
+    "jest",
+    "jsx-a11y",
+    "promise",
+    "simple-import-sort"
+  ],
+
+  rules: {
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "no-console": process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    "padded-blocks": "off",
+    "simple-import-sort/sort": "error"
+  }
+}
