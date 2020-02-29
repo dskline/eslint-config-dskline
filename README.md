62bc58b6f761ce999ad8689ec01ce5d9be99cf5b
Spencer Kline <dskline1@gmail.com>
2020-02-29 13:58:46 -0500

    Update modules and add advanced import sorting

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # eslint-config-dskline
+
+Personal ESLint Configuration for projects at github.com/dskline
+
+### Features
+
+* JavaScript linting with `Standard JS`
+* React best practices (including hooks)
+* Accessibility (a11y) best practices
+* Browser compatibility warnings
+* Typescript integration
+* Import sorting
+
+### Sort Order
+
+This configuration enables automatic import sorting using `eslint --fix` to the following group order:
+
+1. React import
+2. Third party libraries (i.e. from `node_modules`)
+3. Imports from `/src`
+4. Relative imports (i.e. `../` or `./`)
+5. CSS imports
+
+Imports in the same group will then be sorted alphabetically.
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-
 const typescriptDelimiter = {
   delimiter: "comma",
-  requireLast: false,
-}
+  requireLast: false
+};
 
 module.exports = {
-
   parser: "@typescript-eslint/parser",
 
   extends: [
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "eslint:recommended",
     "plugin:compat/recommended",
     "plugin:jest/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:promise/recommended",
-    "plugin:react/recommended",
     "react-app",
     "standard",
-    "standard-react",
+    "standard-react"
   ],
 
   plugins: [
@@ -35,13 +36,43 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/indent": "off",
-    "@typescript-eslint/member-delimiter-style": ["error", {
-      multiline: typescriptDelimiter,
-      singleline: typescriptDelimiter
-    }],
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      {
+        multiline: typescriptDelimiter,
+        singleline: typescriptDelimiter
+      }
+    ],
+    "react/prop-types": "off",
+    "arrow-body-style": ["error", "never"],
     "comma-dangle": ["error", "only-multiline"],
-    "no-console": process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    // TODO: fix conflicts with aliasing in nextjs+typescript+babel+webpack
+    // until then, let typescript handle unresolved imports
+    "import/no-unresolved": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
     "padded-blocks": "off",
-    "simple-import-sort/sort": "error",
+    "simple-import-sort/sort": [
+      "error",
+      {
+        groups: [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          [
+            "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)"
+          ],
+          // Packages. `react` related packages come first.
+          ["^react", "^@?\\w"],
+          // Side effect imports.
+          ["^\\u0000"],
+          // Internal packages.
+          ["^src(/.*|$)"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.s?css$"]
+        ]
+      }
+    ]
   }
-}
+};
