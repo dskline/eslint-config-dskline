7b28f67cdac83bf119f9c6c268029e6fb8dbcba0
Spencer Kline <dskline1@gmail.com>
2021-06-12 10:11:18 -0400

    feat: add tailwind/unicorn linting and other small fixes

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
     "plugin:jsx-a11y/recommended",
     "plugin:promise/recommended",
     "plugin:react/recommended",
+    "plugin:tailwindcss/recommended",
     "plugin:unicorn/recommended",
   ],
 
@@ -32,6 +33,8 @@ module.exports = {
     "jsx-a11y",
     "promise",
     "simple-import-sort",
+    "unicorn",
+    "tailwindcss",
   ],
 
   settings: {
@@ -52,23 +55,18 @@ module.exports = {
     ],
     "arrow-body-style": ["error", "as-needed"],
     "comma-dangle": ["error", "only-multiline"],
-    'unicorn/filename-case': ['off'],
-    'unicorn/prevent-abbreviations': ['off'],
-    'import/no-extraneous-dependencies': [
-      'error',
+    "object-curly-newline": ["error", { consistent: true }],
+    "unicorn/filename-case": ["off"],
+    "unicorn/prevent-abbreviations": ["off"],
+    "import/no-extraneous-dependencies": [
+      "error",
       {
-        devDependencies: [
-          '.storybook/**',
-          'src/**/*.stories.*',
-        ]
-      }
+        devDependencies: [".storybook/**", "src/**/*.stories.*"],
+      },
     ],
     // TODO: fix conflicts with aliasing in nextjs+typescript+babel+webpack
     // until then, let typescript handle unresolved imports
     "import/no-unresolved": "off",
-    // 1: Turn off rules that are no longer necessary in React 17 and in Next.js
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "react/require-default-props": "off",
     "react/jsx-props-no-spreading": [1, { custom: "ignore" }],
